c935384f944798cafe764b7f30a41990c107087b
Svitlana Filippova <s.rubinets@gmail.com>
2024-11-19 16:16:44 +0300

    MenuFragment - начало

--- app/src/main/java/com/mandarinkafe/mandarin/ui/MainActivity.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.mandarinkafe.mandarin
+package com.mandarinkafe.mandarin.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mandarinkafe.mandarin.R
 import com.mandarinkafe.mandarin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
