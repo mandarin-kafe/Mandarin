14165d42e2c558eb7f067d1c1d47c17bd5afcd3c
Svitlana Filippova <s.rubinets@gmail.com>
2024-11-20 16:43:53 +0300

    MainActivity - подготовлена вёрстка тулбара и контейнера для фрагментов; MenuFragment - подготовлена вёрстка, настроен RecycleView; созданы модели данных в Domain и мок-списки

---
+++ app/src/main/java/com/mandarinkafe/mandarin/domain/models/Meal.kt
@@ -0,0 +1,405 @@
+package com.mandarinkafe.mandarin.domain.models
+
+data class Meal(
+    val id: String,                // Уникальный идентификатор блюда
+    val name: String,              // Название блюда
+    val description: String?,      // Описание блюда
+    val weight: Int?,              // Вес блюда в граммах
+    val price: Int,                // Цена блюда
+    val imageUrl: String,          // Ссылка на изображение блюда
+    val category: String,          // Категория блюда
+    val isVegetarian: Boolean,     // Флаг: является ли блюдо вегетарианским
+    val isFavorite: Boolean,     // Флаг: есть ли блюдо в списка избранных (инфо из Shared Prefs)
+)
+
+
+var mockPizzaList = arrayListOf<Meal>(
+    Meal(
+        "1",
+        "МАРГАРИТА",
+        "Томатный соус, помидоры, моцарелла, орегано и базилик",
+        490,
+        585,
+        "https://optim.tildacdn.com/tild3064-3131-4362-b537-366634323165/-/resize/312x/-/format/webp/margaritta_veg.jpg",
+        "pizza",
+        true,
+        false
+    ),
+    Meal(
+        "2",
+        "МАНДАРИН",
+        "Моцарелла, сливочно-чесночный соус, шампиньоны, вяленые томаты, соус песто с грецким орехом, укроп, мягкая моцарелла",
+        670,
+        985,
+        "https://optim.tildacdn.com/tild3666-6364-4133-b961-346132373737/-/resize/312x/-/format/webp/mandarin_new.jpg",
+        "pizza",
+        true,
+        false
+    ),
+    Meal(
+        "3",
+        "ДОЛЬЧЕ ВИТА",
+        "Сливочный соус, груша, моцарелла, горгонзола, грецкие орехи, мед",
+        650,
+        1050,
+        "https://optim.tildacdn.com/tild6461-3330-4761-b163-616164303634/-/resize/312x/-/format/webp/dolce_vita_new.jpg",
+        "pizza",
+        true,
+        false
+    ),
+    Meal(
+        "4",
+        "ОХОТНИЧЬЯ",
+        "Томатный соус, ветчина, охотничьи колбаски, шампиньоны, маринованные огурцы, моцарелла",
+        580,
+        770,
+        "https://optim.tildacdn.com/tild3830-3031-4662-b131-326163643766/-/resize/312x/-/format/webp/ohotnichiya_new.jpg",
+        "pizza",
+        false,
+        false
+    ),
+    Meal(
+        "5",
+        "МОРЕ ВНУТРИ",
+        "Томатный соус, семга, морской окунь, креветки, каперсы, маслины, красный лук, моцарелла, бальзамический крем",
+        620,
+        1250,
+        "https://optim.tildacdn.com/tild6361-6536-4566-b462-643731386235/-/resize/312x/-/format/webp/more_vnutri_new.jpg",
+        "pizza",
+        false,
+        false
+    ),
+    Meal(
+        "1",
+        "МАРГАРИТА",
+        "Томатный соус, помидоры, моцарелла, орегано и базилик",
+        490,
+        585,
+        "https://optim.tildacdn.com/tild3064-3131-4362-b537-366634323165/-/resize/312x/-/format/webp/margaritta_veg.jpg",
+        "pizza",
+        true,
+        true
+    ),
+    Meal(
+        "2",
+        "МАНДАРИН",
+        "Моцарелла, сливочно-чесночный соус, шампиньоны, вяленые томаты, соус песто с грецким орехом, укроп, мягкая моцарелла",
+        670,
+        985,
+        "https://optim.tildacdn.com/tild3666-6364-4133-b961-346132373737/-/resize/312x/-/format/webp/mandarin_new.jpg",
+        "pizza",
+        true,
+        false
+    ),
+    Meal(
+        "3",
+        "ДОЛЬЧЕ ВИТА",
+        "Сливочный соус, груша, моцарелла, горгонзола, грецкие орехи, мед",
+        650,
+        1050,
+        "https://optim.tildacdn.com/tild6461-3330-4761-b163-616164303634/-/resize/312x/-/format/webp/dolce_vita_new.jpg",
+        "pizza",
+        true,
+        false
+    ),
+    Meal(
+        "4",
+        "ОХОТНИЧЬЯ",
+        "Томатный соус, ветчина, охотничьи колбаски, шампиньоны, маринованные огурцы, моцарелла",
+        580,
+        770,
+        "https://optim.tildacdn.com/tild3830-3031-4662-b131-326163643766/-/resize/312x/-/format/webp/ohotnichiya_new.jpg",
+        "pizza",
+        false,
+        true
+    ),
+    Meal(
+        "5",
+        "МОРЕ ВНУТРИ",
+        "Томатный соус, семга, морской окунь, креветки, каперсы, маслины, красный лук, моцарелла, бальзамический крем",
+        620,
+        1250,
+        "https://optim.tildacdn.com/tild6361-6536-4566-b462-643731386235/-/resize/312x/-/format/webp/more_vnutri_new.jpg",
+        "pizza",
+        false,
+        false
+    ),
+    Meal(
+        "1",
+        "МАРГАРИТА",
+        "Томатный соус, помидоры, моцарелла, орегано и базилик",
+        490,
+        585,
+        "https://optim.tildacdn.com/tild3064-3131-4362-b537-366634323165/-/resize/312x/-/format/webp/margaritta_veg.jpg",
+        "pizza",
+        true,
+        false
+    ),
+    Meal(
+        "2",
+        "МАНДАРИН",
+        "Моцарелла, сливочно-чесночный соус, шампиньоны, вяленые томаты, соус песто с грецким орехом, укроп, мягкая моцарелла",
+        670,
+        985,
+        "https://optim.tildacdn.com/tild3666-6364-4133-b961-346132373737/-/resize/312x/-/format/webp/mandarin_new.jpg",
+        "pizza",
+        true,
+        false
+    ),
+    Meal(
+        "3",
+        "ДОЛЬЧЕ ВИТА",
+        "Сливочный соус, груша, моцарелла, горгонзола, грецкие орехи, мед",
+        650,
+        1050,
+        "https://optim.tildacdn.com/tild6461-3330-4761-b163-616164303634/-/resize/312x/-/format/webp/dolce_vita_new.jpg",
+        "pizza",
+        true,
+        true
+    ),
+    Meal(
+        "4",
+        "ОХОТНИЧЬЯ",
+        "Томатный соус, ветчина, охотничьи колбаски, шампиньоны, маринованные огурцы, моцарелла",
+        580,
+        770,
+        "https://optim.tildacdn.com/tild3830-3031-4662-b131-326163643766/-/resize/312x/-/format/webp/ohotnichiya_new.jpg",
+        "pizza",
+        false,
+        false
+    ),
+    Meal(
+        "5",
+        "МОРЕ ВНУТРИ",
+        "Томатный соус, семга, морской окунь, креветки, каперсы, маслины, красный лук, моцарелла, бальзамический крем",
+        620,
+        1250,
+        "https://optim.tildacdn.com/tild6361-6536-4566-b462-643731386235/-/resize/312x/-/format/webp/more_vnutri_new.jpg",
+        "pizza",
+        false,
+        false
+    ),
+)
+var mockBurgerList = arrayListOf<Meal>(
+    Meal(
+        "31",
+        "ДАКОТА",
+        "✓ говяжья котлета\n" +
+                "✓ сыр чеддер\n" +
+                "✓ бекон\n" +
+                "✓ лук фри\n" +
+                "✓ маринованные огурцы\n" +
+                "✓ помидоры\n" +
+                "✓ соус сальса\n" +
+                "✓ соус тартар",
+        null,
+        595,
+        "https://optim.tildacdn.com/tild3763-6634-4137-a662-383034323938/-/resize/312x/-/format/webp/_-2.jpg",
+        "burger",
+        false,
+        true
+    ),
+    Meal(
+        "32",
+        "ХАМБУРГЕР",
+        "✓ булка\n" +
+                "✓ говяжья котлета\n" +
+                "✓ сыр чеддер\n" +
+                "✓ соус микс барбекю",
+        null,
+        455,
+        "https://optim.tildacdn.com/tild6337-3237-4264-a535-333266346235/-/resize/312x/-/format/webp/_-2.jpg",
+        "burger",
+        false,
+        false
+    ),
+    Meal(
+        "33",
+        "ДЗЕН",
+        "✓ котлета из нута\n" +
+                "✓ сыр чеддер\n" +
+                "✓ айсберг\n" +
+                "✓ красный лук\n" +
+                "✓ помидоры\n" +
+                "✓ соус сальса\n" +
+                "✓ соус максибургер",
+        null,
+        425,
+        "https://optim.tildacdn.com/tild6631-3532-4535-b334-666436313734/-/resize/312x/-/format/webp/_-2.jpg",
+        "burger",
+        true,
+        false
+    ),
+
+    )
+var mockSushiList = arrayListOf<Meal>(
+    Meal(
+        "11",
+        "КАРМА",
+        "Рис, нори, снежный краб, яки соус, огурец, угорь, семга, унаги соус, лайм, икра тобико оранжевая, микрозелень",
+        216,
+        670,
+        "https://optim.tildacdn.com/tild6536-6636-4062-a336-356138393836/-/resize/312x/-/format/webp/karma.jpg",
+        "sushi",
+        false,
+        true
+    ),
+    Meal(
+        "12",
+        "СУТРА",
+        "Нори, рис, творожный сыр, авокадо, икра тобико оранжевая, семга, шичими, манго соус, майонез",
+        210,
+        890,
+        "https://optim.tildacdn.com/tild3131-3139-4538-b366-656139646633/-/resize/312x/-/format/webp/sutra.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "15",
+        "МАНДАРИН",
+        "Нори, рис, болгарский перец, жареный лук, огурец, сливочный сыр",
+        null,
+        310,
+        "https://optim.tildacdn.com/tild3935-3437-4462-b535-646633393239/-/resize/312x/-/format/webp/mandarin_veg.jpg",
+        "sushi",
+        true,
+        true
+
+    ),
+    Meal(
+        "13",
+        "ШАМБАЛА",
+        "Нори, рис, творожный сыр, омлет, авокадо, манго соус, майонез, семга, сахар коричневый, лайм",
+        220,
+        810,
+        "https://optim.tildacdn.com/tild6330-6464-4736-a662-346566613433/-/resize/312x/-/format/webp/shambala.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "14",
+        "ЧАКРА",
+        "Нори, рис, маринованный лосось, яки соус, снежный краб, авокадо, огурец",
+        225,
+        770,
+        "https://optim.tildacdn.com/tild3838-3964-4938-b638-343236336364/-/resize/312x/-/format/webp/chakra.jpg",
+        "sushi",
+        false,
+        true
+    ),
+    Meal(
+        "11",
+        "КАРМА",
+        "Рис, нори, снежный краб, яки соус, огурец, угорь, семга, унаги соус, лайм, икра тобико оранжевая, микрозелень",
+        216,
+        670,
+        "https://optim.tildacdn.com/tild6536-6636-4062-a336-356138393836/-/resize/312x/-/format/webp/karma.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "12",
+        "СУТРА",
+        "Нори, рис, творожный сыр, авокадо, икра тобико оранжевая, семга, шичими, манго соус, майонез",
+        210,
+        890,
+        "https://optim.tildacdn.com/tild3131-3139-4538-b366-656139646633/-/resize/312x/-/format/webp/sutra.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "15",
+        "МАНДАРИН",
+        "Нори, рис, болгарский перец, жареный лук, огурец, сливочный сыр",
+        null,
+        310,
+        "https://optim.tildacdn.com/tild3935-3437-4462-b535-646633393239/-/resize/312x/-/format/webp/mandarin_veg.jpg",
+        "sushi",
+        true,
+        true
+
+    ),
+    Meal(
+        "13",
+        "ШАМБАЛА",
+        "Нори, рис, творожный сыр, омлет, авокадо, манго соус, майонез, семга, сахар коричневый, лайм",
+        220,
+        810,
+        "https://optim.tildacdn.com/tild6330-6464-4736-a662-346566613433/-/resize/312x/-/format/webp/shambala.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "14",
+        "ЧАКРА",
+        "Нори, рис, маринованный лосось, яки соус, снежный краб, авокадо, огурец",
+        225,
+        770,
+        "https://optim.tildacdn.com/tild3838-3964-4938-b638-343236336364/-/resize/312x/-/format/webp/chakra.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "11",
+        "КАРМА",
+        "Рис, нори, снежный краб, яки соус, огурец, угорь, семга, унаги соус, лайм, икра тобико оранжевая, микрозелень",
+        216,
+        670,
+        "https://optim.tildacdn.com/tild6536-6636-4062-a336-356138393836/-/resize/312x/-/format/webp/karma.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "12",
+        "СУТРА",
+        "Нори, рис, творожный сыр, авокадо, икра тобико оранжевая, семга, шичими, манго соус, майонез",
+        210,
+        890,
+        "https://optim.tildacdn.com/tild3131-3139-4538-b366-656139646633/-/resize/312x/-/format/webp/sutra.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "15",
+        "МАНДАРИН",
+        "Нори, рис, болгарский перец, жареный лук, огурец, сливочный сыр",
+        null,
+        310,
+        "https://optim.tildacdn.com/tild3935-3437-4462-b535-646633393239/-/resize/312x/-/format/webp/mandarin_veg.jpg",
+        "sushi",
+        true,
+        true
+
+    ),
+    Meal(
+        "13",
+        "ШАМБАЛА",
+        "Нори, рис, творожный сыр, омлет, авокадо, манго соус, майонез, семга, сахар коричневый, лайм",
+        220,
+        810,
+        "https://optim.tildacdn.com/tild6330-6464-4736-a662-346566613433/-/resize/312x/-/format/webp/shambala.jpg",
+        "sushi",
+        false,
+        false
+    ),
+    Meal(
+        "14",
+        "ЧАКРА",
+        "Нори, рис, маринованный лосось, яки соус, снежный краб, авокадо, огурец",
+        225,
+        770,
+        "https://optim.tildacdn.com/tild3838-3964-4938-b638-343236336364/-/resize/312x/-/format/webp/chakra.jpg",
+        "sushi",
+        false,
+        false
+    ),
+
+    )
--- app/src/main/java/com/mandarinkafe/mandarin/ui/menu/MenuAdapter.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/ui/menu/MenuAdapter.kt
@@ -1,34 +1,106 @@
 package com.mandarinkafe.mandarin.ui.menu
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.mandarinkafe.mandarin.R
 import com.mandarinkafe.mandarin.databinding.ListMenuItemBinding
-import com.mandarinkafe.mandarin.domain.Meal
+import com.mandarinkafe.mandarin.domain.models.Meal
+
+import com.mandarinkafe.mandarin.domain.models.MenuItem
 
 
-class MenuAdapter(private val onMealClick: (meal: Meal) -> Unit) :
-    RecyclerView.Adapter<MenuAdapter.MenuHolder>() {
-    private var mealList: ArrayList<Meal> = arrayListOf()
+class MenuAdapter(
+    private val items: List<MenuItem>,
+    private val clickListener: MealClickListener
+) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     private var isClickAllowed = true
     private val handler = Handler(Looper.getMainLooper())
 
 
-    class MenuHolder(private val parentView: View) : RecyclerView.ViewHolder(parentView) {
-        private val binding = ListMenuItemBinding.bind(parentView)
+    override fun getItemViewType(position: Int): Int {
+        return when (items[position]) {
+            is MenuItem.Header -> VIEW_TYPE_HEADER
+            is MenuItem.MealItem -> VIEW_TYPE_MEAL
+        }
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_HEADER -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.list_menu_header, parent, false)
+                HeaderViewHolder(view)
+            }
+
+            VIEW_TYPE_MEAL -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.list_menu_item, parent, false)
+                MealViewHolder(view, clickListener)
+            }
+
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (val item = items[position]) {
+            is MenuItem.Header -> (holder as HeaderViewHolder).bind(item)
+            is MenuItem.MealItem -> {
+                (holder as MealViewHolder).bind(item.meal)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    private fun clickDebounce(): Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
+    }
+
+    private fun onMealClickListener(meal: Meal) {
+        if (clickDebounce()) {
+            clickListener.onMealClick(meal)
+        }
+    }
+
+    class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val headerTextView: TextView = itemView.findViewById(R.id.tv_category_title)
+
+        fun bind(header: MenuItem.Header) {
+            headerTextView.text = header.categoryName
+        }
+    }
+
+
+    class MealViewHolder(
+        private val parentView: View,
+        private val clickListener: MealClickListener
+    ) :
+        RecyclerView.ViewHolder(parentView) {
+        private val binding = ListMenuItemBinding.bind(parentView)
 
         fun bind(meal: Meal) = with(binding) {
             val cornerRadius =
-                this@MenuHolder.parentView.resources.getDimensionPixelSize(R.dimen.image_corner_radius_2)
-
+                this@MealViewHolder.parentView.resources.getDimensionPixelSize(R.dimen.image_corner_radius_2)
             tvMealTitle.text = meal.name
 
             if (meal.description.isNullOrEmpty()) {
@@ -43,62 +115,71 @@ class MenuAdapter(private val onMealClick: (meal: Meal) -> Unit) :
                 tvMealWeight.text = meal.weight.toString() + " г"
             }
 
-            tvMealPrice.text = meal.price.toString() + " ₽"
+            btAddToCartPrice.text = meal.price.toString() + " ₽"
 
             Glide.with(parentView)
                 .load(meal.imageUrl)
                 .centerCrop()
-                .transform(
-                    RoundedCorners(
-                        cornerRadius
-                    )
-                )
+                .transform(RoundedCorners(cornerRadius))
                 .placeholder(R.drawable.ic_cover_placeholder)
                 .into(ivMealPicture)
             tvMealIngredients.requestLayout()
+            ivAddToFavorite.setImageDrawable(getFavoriteDrawable(meal.isFavorite))
+
+
+            itemView.setOnClickListener { clickListener.onMealClick(meal) }
+            ivAddToFavorite.setOnClickListener { clickListener.onFavoriteToggleClick(meal) }
+            btAddToCartPrice.setOnClickListener {
+                clickListener.onAddToCartClick(meal)
+                extraCartButtonsManager(VisibilityStatus.VISIBLE)
+            }
+            btCartMinus.setOnClickListener { clickListener.minusToCartClick(meal) }
+            btCartPlus.setOnClickListener { clickListener.plusToCartClick(meal) }
         }
 
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_menu_item, parent, false)
-        return MenuHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: MenuHolder, position: Int) {
-        val meal = mealList[position]
-        holder.bind(meal)
-        holder.itemView.setOnClickListener { onClickListener(meal) }
-    }
-
-    override fun getItemCount(): Int {
-        return mealList.size
-    }
-
-    private fun clickDebounce(): Boolean {
-        val current = isClickAllowed
-        if (isClickAllowed) {
-            isClickAllowed = false
-            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        @SuppressLint("UseCompatLoadingForDrawables")
+        private fun getFavoriteDrawable(inFavorite: Boolean): Drawable? {
+            return itemView.context.getDrawable(
+                if (inFavorite) R.drawable.ic_favorite_active else R.drawable.ic_favorite_inactive
+            )
         }
-        return current
-    }
 
+        private fun extraCartButtonsManager(status: VisibilityStatus) {
+            when (status) {
+                VisibilityStatus.VISIBLE -> binding.apply {
+                    btAddToCartPrice.visibility = View.GONE
+                    btCartMinus.visibility = View.VISIBLE
+                    btCartPlus.visibility = View.VISIBLE
+                }
 
-    private fun onClickListener(meal: Meal) {
-        if (clickDebounce()) {
-            onMealClick(meal)
+                VisibilityStatus.HIDDEN -> binding.apply {
+                    btAddToCartPrice.visibility = View.VISIBLE
+                    btCartMinus.visibility = View.GONE
+                    btCartPlus.visibility = View.GONE
+                }
+            }
+
 
         }
     }
 
-    fun submitList(mealList: ArrayList<Meal>) {
-        this.mealList = mealList
+    interface MealClickListener {
+        fun onMealClick(meal: Meal)
+        fun onFavoriteToggleClick(meal: Meal)
+        fun onAddToCartClick(meal: Meal)
+        fun plusToCartClick(meal: Meal)
+        fun minusToCartClick(meal: Meal)
     }
 
-    companion object {
-        private const val CLICK_DEBOUNCE_DELAY = 1000L
+    private companion object {
+        private const val CLICK_DEBOUNCE_DELAY = 30L
+        const val VIEW_TYPE_HEADER = 0
+        const val VIEW_TYPE_MEAL = 1
+
+        enum class VisibilityStatus {
+            VISIBLE,
+            HIDDEN
+        }
     }
 
 }
