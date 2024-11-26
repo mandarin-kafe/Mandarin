3b3465b92ddc404e5a8e2d4639eb08714a3f908b
SergeySh97 <Sergey_Sh97@mail.ru>
2024-11-26 14:34:25 +0300

    Первичная настройка Navigation Drawer Code cleanup

--- app/src/main/java/com/mandarinkafe/mandarin/ui/MainActivity.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/ui/MainActivity.kt
@@ -2,22 +2,51 @@ package com.mandarinkafe.mandarin.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.mandarinkafe.mandarin.R
 import com.mandarinkafe.mandarin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = requireNotNull(_binding) { "Binding wasn't initialized" }
+
+    private var _appBarConfiguration: AppBarConfiguration? = null
+    private val appBarConfiguration: AppBarConfiguration
+        get() = requireNotNull(_appBarConfiguration) { "App Bar Configuration wasn't initialized" }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeUI()
+    }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    private fun initializeUI() {
+        setSupportActionBar(binding.appBarMain.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val drawerLayout: DrawerLayout = binding.drawerLayoutMain
+        val navView: NavigationView = binding.navView
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.container_view) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        _appBarConfiguration = AppBarConfiguration(
+            navController.graph, drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.container_view)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
